8e7054c55e55ae4cd6834ba6dee36f2d15c0ac2c
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2024-05-24 14:44:38 +0300

    setup 2

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import { DndContext, closestCorners } from "@dnd-kit/core"
+import { Column } from './assets/compunents/Column'
+function App() {
+  const [tasks, setTasks] = useState([
+    {id: 1, title: "add tasks"}, 
+    {id: 2, title: "add tasks"}, 
+    {id: 3, title: "add tasks to 2"},
+    ])
+  return (
+    <>
+    
+
+      <div><p>Hi</p></div>
+      <DndContext collisionDetection={closestCorners}><Column tasks={tasks}/></DndContext>
+    </>
+  )
+}
+
+export default App
